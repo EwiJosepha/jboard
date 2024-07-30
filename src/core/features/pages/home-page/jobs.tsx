d43e03d662ac2cd609d3e74718d23314d42dcf71
ewi josepha <ewijosepha@gmail.com>
2024-07-30 19:31:22 +0200

    implementing radix ui

--- src/core/features/pages/home-page/jobs.tsx
+++ src/core/features/pages/home-page/jobs.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import Jobrow from './jobrow'
+import { Box, Flex, TextArea, TextField, Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css';
 
 function Jobs() {
   return (
-    <div className='bg-gray-200 py-4 rounded-xl'>
-      <div className='container'>
-        <h2 className=' font-bold text-1xl'>Recent Jobs</h2>
-        <Jobrow />
+    <>
+      <div className='bg-gray-200 py-4 rounded-xl'>
+        <div className='container'>
+          <h2 className=' font-bold text-1xl'>Recent Jobs</h2>
+          <Jobrow />
+        </div>
+
       </div>
-    </div>
+      <Flex direction="column" gap="3">
+      <Theme className='flex flex-col gap-3 pt-10'>
+        <TextField.Root placeholder="Job Title"  radius='large'/>
+        <TextArea placeholder='job description '  resize={'vertical'} radius='large'/>
+        </Theme>
+      </Flex>
+    </>
   )
 }
 
