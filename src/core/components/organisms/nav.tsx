cdf3441882381f316b0c21dbbff870a0316f2bcd
ewi josepha <ewijosepha@gmail.com>
2024-07-31 00:37:30 +0200

    almost  done from building the component for posting job

--- src/core/components/organisms/nav.tsx
+++ src/core/components/organisms/nav.tsx
@@ -7,9 +7,8 @@ function NavBar() {
       <div className=" container flex justify-between py-4 ">
         <Link href={'/'} className="font-bold text-lg" >Job Board</Link>
         <nav className=" flex gap-2  *:p-2 *:rounded-md">
-          <Link  href={"/"} className="*:bg-gray-200">Login</Link>
+          <Link  href={"/sig"} className="*:bg-gray-200">Login</Link>
           <Link href={"/"} className="bg-blue-600 text-white" >Post a Job</Link>
-          <Link href={"/"}>hey</Link>
         </nav>
       </div>
     </header>
