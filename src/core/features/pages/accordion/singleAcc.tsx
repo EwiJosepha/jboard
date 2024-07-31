abc3885a4b6ca1371638b26cef1b73e6111f76a9
ewi josepha <ewijosepha@gmail.com>
2024-08-01 00:44:46 +0200

    accordian copleted.

--- src/core/features/pages/accordion/singleAcc.tsx
+++ src/core/features/pages/accordion/singleAcc.tsx
@@ -1,36 +1,58 @@
 "use client"
 import React, { useState } from 'react'
 import { data } from '@/core/components/atoms/accordionDummy'
-import { Divide, Plus } from 'lucide-react'
+import { Minus, Plus } from 'lucide-react'
 
 function SingleAcc() {
-  const [selected, setSelected] = useState(null)
-  const[enablemultiSelect, setEnablemultiSelect] = useState(false)
-  const[multipleSelectArray, setMultiSelectArray] = useState([])
+  const [selected, setSelected] = useState([])
+  const [enablemultiSelect, setEnableMultiSelect] = useState(false)
 
   function handleSingleClick(id) {
-    setSelected(id === selected ? "" : id)
+    if (enablemultiSelect) {
+      if (selected.includes(id)) {
+        setSelected(selected.filter((filtedId) =>
+          filtedId !== id
+        ))
+      } else {
+              setSelected([...selected, id])
+      }
+    } else {
+      setSelected(id === selected[0] ? [] : [id])
+
+    }
   }
 
-  function handlmulti () {
-  setEnablemultiSelect(true)
+  console.log(selected[0] , "suppose to be an array");
+  console.log(selected);
   
-   
+  
+
+  function handleMultiSelect() {
+    setEnableMultiSelect(true)
   }
+
+
 
   return (
     <div className='pt-10'>
-           <button className='p-3 bg-black cursor-pointer  text-white items-center' onClick={()=>{handlmulti}}>Enable multiselection</button>
+      <button className='p-3 bg-black cursor-pointer  text-white items-center' onClick={handleMultiSelect}>Enable multiselection</button>
       {data.map((dataitem) => (
         <div className=" p-6 flex flex-col gap-4">
-     
+
 
           <div className='flex flex-col  bg-red-900 p-6 cursor-pointer' onClick={() => handleSingleClick(dataitem.id)}>
             <div className='flex justify-between'>
               <h3 className='text-white'>{dataitem.title}</h3>
-              <Plus className='text-white cursor-pointer' />
+              {selected[0] === dataitem.id ? (
+                <Minus className='text-white cursor-pointer' />
+
+              ) : (
+                <Plus className='text-white cursor-pointer' />
+
+
+              )}
             </div>
-            {selected === dataitem.id ? <div className='text-white'>
+            {selected[0] === dataitem.id ? <div className='text-white'>
               {dataitem.description}
             </div> :
               null
